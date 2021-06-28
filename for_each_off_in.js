072e5d81e8547da61fd6fb2d00ade30c5b6315cc
vaibhav singh <61928952+Titansingh@users.noreply.github.com>
2021-06-28 19:39:14 +0530

    backup

--- for_each_off_in.js
+++ for_each_off_in.js
@@ -1,3 +1,19 @@
 var a = ['car','car1','car2','car3',2,3,4,5];
 
-a.forEach((a)=> console.log(a));
+a.forEach((a)=> console.log(a));//for each
+
+for (const item of a) {     //for off
+    console.log(item + ' 1');
+    
+}
+
+const symbol = {
+    yt: "youtube",
+    fb: "facebook",
+    ig: "instagram"
+}
+
+for (const key in symbol) {
+    console.log(symbol[key]);
+    
+}
