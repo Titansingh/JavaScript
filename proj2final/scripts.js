6c3ed072961c61b206151757b68acdac3ab4de48
vaibhav singh <61928952+Titansingh@users.noreply.github.com>
2021-07-06 16:40:09 +0530

    final push

--- proj2final/scripts.js
+++ proj2final/scripts.js
@@ -1,10 +1,10 @@
 
-const colorName = [".red",".cyan",".violet",".orange",".pink"]
+// const colorName = [".red",".cyan",".violet",".orange",".pink"]
 
-colorName.forEach((item) => {
-  document.querySelector(item).addEventListener("mouseenter",() =>{
-    document.querySelector(".center").style.backgroundColor = window.getComputedStyle(document.querySelector(item)).backgroundColor;})
-});
+// colorName.forEach((item) => {
+//   document.querySelector(item).addEventListener("mouseenter",() =>{
+//     document.querySelector(".center").style.backgroundColor = window.getComputedStyle(document.querySelector(item)).backgroundColor;})
+// });
 
 
 
